ff4434deaaff1bd609bf358eebd0a7eab7fa0c9b
Parth Mangtani <impmmmm@gmail.com>
2021-10-31 20:12:18 +0530

    formatting

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import Card from "./Card";
 import "./stylesheets/App.css";
 
 class App extends Component {
-  addCss = () => {
-    const App = document.querySelector(".App");
-    App.style.height = "100vh";
-  };
-  removeCss = () => {
-    const App = document.querySelector(".App");
-    App.style.height = "inherit";
-  };
-  render = () => {
-    return (
-      <div className="App">
-        <Card addCss={this.addCss} removeCss={this.removeCss} />
-      </div>
-    );
-  };
+	addCss = () => {
+		const App = document.querySelector(".App");
+		App.style.height = "100vh";
+	};
+	removeCss = () => {
+		const App = document.querySelector(".App");
+		App.style.height = "inherit";
+	};
+	render = () => {
+		return (
+			<div className="App">
+				<Card addCss={this.addCss} removeCss={this.removeCss} />
+			</div>
+		);
+	};
 }
 
 export default App;
