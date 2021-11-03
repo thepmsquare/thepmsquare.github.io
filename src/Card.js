f61571179fd17d9f5ce4bea609232c0ea1c8a432
Parth Mangtani <impmmmm@gmail.com>
2021-11-03 22:43:18 +0530

    finalise layout for initial load

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
+import {
+	IconButton,
+	Typography,
+	CardActionArea,
+	CardActions,
+	CardContent,
+	Card as MUICard,
+} from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import "./stylesheets/Card.css";
 
 class Card extends Component {
 	render() {
 		return (
-			<div className="Card">
-				<h1>Card</h1>
-			</div>
+			<MUICard className="Card" square>
+				<CardContent className="Card-content">
+					<span className="Card-title">thePmSquare</span>
+					<div className="Card-iconsContainer">
+						<IconButton>
+							<EmailIcon />
+						</IconButton>
+						<IconButton>
+							<GitHubIcon />
+						</IconButton>
+					</div>
+				</CardContent>
+				<CardActionArea className="Card-actionArea">
+					<CardActions className="Card-actions">
+						<Typography variant="button">more</Typography>
+					</CardActions>
+				</CardActionArea>
+			</MUICard>
 		);
 	}
 }
